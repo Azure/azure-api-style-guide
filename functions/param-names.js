9823c72262fc923c10cf044be8b5ab29f1ac5642
Mark Weitzel <markweitzel@microsoft.com>
2021-09-28 10:23:42 -0400

    fixed

--- functions/param-names.js
+++ functions/param-names.js
@@ -14,15 +14,13 @@ module.exports = (targetVal, _opts, paths) => {
     return [];
   }
 
-  if (['path', 'query'].includes(targetVal.in) && targetVal.name !== 'api-version') {
-    if (targetVal.name.match(/^[$@]/)) {
-      return [
-        {
-          message: `Parameter name "${targetVal.name}" should not begin with '$' or '@'.`,
-          path: [...path, 'name'],
-        },
-      ];
-    }
+  if (targetVal.name.match(/^[$@]/)) {
+    return [
+      {
+        message: `Parameter name "${targetVal.name}" should not begin with '$' or '@'.`,
+        path: [...path, 'name'],
+      },
+    ];
   }
   if (['path', 'query'].includes(targetVal.in) && targetVal.name !== 'api-version') {
     if (!targetVal.name.match(/^[a-z][a-z0-9]*([A-Z][a-z0-9]+)*$/)) {
