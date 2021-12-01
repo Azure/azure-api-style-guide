0cca5751f703e414bb670a462aaa489afa9e2f21
Mike Kistler <mikekistler@microsoft.com>
2021-12-01 12:12:22 -0600

    Fix additional-properties-object rule

---
+++ test/additional-properties-object.test.js
@@ -0,0 +1,73 @@
+const { linterForRule } = require('./utils');
+
+let linter;
+
+beforeAll(async () => {
+  linter = await linterForRule('az-additional-properties-object');
+  return linter;
+});
+
+test('az-additional-properties-object should find errors', () => {
+  const oasDoc = {
+    swagger: '2.0',
+    definitions: {
+      This: {
+        description: 'This',
+        type: 'object',
+        additionalProperties: {
+          type: 'object',
+        },
+      },
+      That: {
+        description: 'That',
+        type: 'object',
+        properties: {
+          params: {
+            additionalProperties: {
+              type: 'object',
+            },
+          },
+        },
+      },
+    },
+  };
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(2);
+    expect(results[0].path.join('.')).toBe('definitions.This.additionalProperties');
+    expect(results[1].path.join('.')).toBe('definitions.That.properties.params.additionalProperties');
+  });
+});
+
+test('az-additional-properties-object should find no errors', () => {
+  const oasDoc = {
+    swagger: '2.0',
+    definitions: {
+      This: {
+        description: 'This',
+        type: 'object',
+        additionalProperties: {
+          type: 'string',
+          properties: {
+            prop1: {
+              type: 'string',
+            },
+          },
+        },
+      },
+      That: {
+        description: 'That',
+        type: 'object',
+        properties: {
+          params: {
+            additionalProperties: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+  };
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(0);
+  });
+});
