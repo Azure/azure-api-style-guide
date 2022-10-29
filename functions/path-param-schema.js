24682f07a4dff9740e78037d9a860c2f5cb04626
Mike Kistler <mikekistler@microsoft.com>
2022-10-28 21:44:14 -0500

    Improvements to az-path-parameter-schema

--- functions/path-param-schema.js
+++ functions/path-param-schema.js
@@ -1,12 +1,12 @@
 const URL_MAX_LENGTH = 2083;
 
 // `given` is a (resolved) parameter entry at the path or operation level
-module.exports = (param, _opts, paths) => {
+module.exports = (param, _opts, context) => {
   if (param === null || typeof param !== 'object') {
     return [];
   }
 
-  const path = paths.path || paths.target || [];
+  const path = context.path || context.target || [];
 
   // These errors will be caught elsewhere, so silently ignore here
   if (!param.in || !param.name) {
@@ -30,6 +30,21 @@ module.exports = (param, _opts, paths) => {
     });
   }
 
+  // Only check constraints for the final path parameter on a put or patch that returns a 201
+  const apiPath = path[1] ?? '';
+  if (!apiPath.endsWith(`{${param.name}}`)) {
+    return errors;
+  }
+  if (!['put', 'patch'].includes(path[2] ?? '')) {
+    return errors;
+  }
+
+  const oasDoc = context.document.data;
+  const { responses } = oasDoc.paths[apiPath][path[2]];
+  if (!responses || !responses['201']) {
+    return errors;
+  }
+
   if (!schema.maxLength && !schema.pattern) {
     errors.push({
       message: 'Path parameter should specify a maximum length (maxLength) and characters allowed (pattern).',
@@ -43,7 +58,7 @@ module.exports = (param, _opts, paths) => {
   } else if (schema.maxLength && schema.maxLength >= URL_MAX_LENGTH) {
     errors.push({
       message: `Path parameter maximum length should be less than ${URL_MAX_LENGTH}`,
-      path,
+      path: [...path, 'maxLength'],
     });
   } else if (!schema.pattern) {
     errors.push({
