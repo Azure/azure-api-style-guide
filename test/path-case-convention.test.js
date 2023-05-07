65c0f50151cb55e41b09c95bb5d501ded8fabf3a
Mike Kistler <mikekistler@microsoft.com>
2023-05-06 17:19:07 -0700

    Action in path does not need to be kebab-case

--- test/path-case-convention.test.js
+++ test/path-case-convention.test.js
@@ -13,14 +13,12 @@ test('az-path-case-convention should find errors', () => {
     paths: {
       '/fooBar': {},
       '/foo-bar/{id}/barBaz': {},
-      '/foo/{bar}:bazQux': {},
     },
   };
   return linter.run(oasDoc).then((results) => {
-    expect(results.length).toBe(3);
+    expect(results.length).toBe(2);
     expect(results[0].path.join('.')).toBe('paths./fooBar');
     expect(results[1].path.join('.')).toBe('paths./foo-bar/{id}/barBaz');
-    expect(results[2].path.join('.')).toBe('paths./foo/{bar}:bazQux');
   });
 });
 
@@ -32,7 +30,7 @@ test('az-path-case-convention should find no errors', () => {
       '/:foobar': {},
       '/abcdefghijklmnopqrstuvwxyz0123456789': {},
       '/a0-b1-c2/d3-e4-f5/ghi-jkl-mno/pqrstuvwxyz': {},
-      '/foo/{$#@&^}:bar-baz': {},
+      '/foo/{bar}:bazQux': {},
     },
   };
   return linter.run(oasDoc).then((results) => {
